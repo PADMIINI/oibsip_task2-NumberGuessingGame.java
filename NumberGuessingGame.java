b378b05dcae11e4f5a1c7327d6b5e3d7cc3c23b7
padmini s <vkmini33@gmail.com>
2025-07-03 12:28:06 +0530

    Update NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,96 +1,84 @@
 import java.util.*;
-
 public class NumberGuessingGame {
     static ArrayList<Integer> scoreBoard = new ArrayList<Integer>();
-
     public static void main(String[] args) {
-        NumberGuessingGame game = new NumberGuessingGame();
-        game.menu();
+        NumberGuessingGame methodChange = new NumberGuessingGame();
+        methodChange.menu(scoreBoard);
     }
-
-    public void menu() {
+    public void menu(ArrayList<Integer> scoreBoard) {
+        NumberGuessingGame methodChange = new NumberGuessingGame();
         Scanner input = new Scanner(System.in);
-        while (true) {
-            System.out.println("--------------------");
-            System.out.println("Welcome to the number game");
-            System.out.println("1) Play the Game");
-            System.out.println("2) Score Board");
-            System.out.println("3) Exit the game");
-            System.out.println("--------------------");
+        System.out.println("--------------------");
+        System.out.println("Welcome to the number game");
+        System.out.println("1) Play the Game");
+        System.out.println("2) Score Board");
+        System.out.println("3) Exit the game");
+        System.out.println("--------------------");
+        try {
             System.out.print("What action would you like to do from the above actions? ");
-            try {
-                int menuOption = input.nextInt();
-                switch (menuOption) {
-                    case 1:
-                        System.out.print("\nWhat would you like the range of the numbers to be? ");
-                        int numberRange = input.nextInt();
-                        int randomNumber = randomNumber(numberRange);
-                        guessNumber(randomNumber);
-                        break;
-                    case 2:
-                        displayScoreBoard();
-                        break;
-                    case 3:
-                        System.out.println("\nThanks for playing the game!");
-                        System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Invalid number entry. Please try again.");
-                }
-            } catch (InputMismatchException e) {
-                System.err.println("\nInvalid input. Please enter a number.\n");
-                input.nextLine(); // clear invalid input
+            int menuOption = input.nextInt();
+            switch (menuOption) {
+                case 1:
+                    System.out.print("\n"+"What would you like the range of the numbers to be? ");
+                    int numberRange = input.nextInt();
+                    int randomNumber = methodChange.randomNumber(numberRange);
+                    methodChange.guessNumber(randomNumber);
+                    break;
+                case 2:
+                    methodChange.displayScoreBoard();
+                    break;
+                case 3:
+                    System.out.println("\n"+"Thanks for playing the game!");
+                    System.exit(1);
+                    break;
+                default:
+                    throw new InputMismatchException("Invalid number entry.Could you please Try again later");
             }
+        }catch(InputMismatchException e){
+            System.err.println("\n"+e.getMessage() +"\n");
+            menu(scoreBoard);
         }
     }
-
-    public static int randomNumber(int numberRange) {
+    public int randomNumber(int numberRange) {
         Random random = new Random();
-        return random.nextInt(numberRange) + 1;
+        int randomNumber = random.nextInt(numberRange) + 1;
+        return randomNumber;
     }
-
     public void guessNumber(int randomNumber) {
         Scanner input = new Scanner(System.in);
         int userGuess;
-        int guessCount = 0;
+        int guess = 0;
         do {
             System.out.print("Enter your guess number: ");
-            while (!input.hasNextInt()) {
-                System.out.print("Please enter a valid number: ");
-                input.next();
-            }
             userGuess = input.nextInt();
-            guessCount++;
+            guess++;
             if (userGuess > randomNumber) {
                 System.out.println("Lower");
             } else if (userGuess < randomNumber) {
                 System.out.println("Higher");
             }
         } while (randomNumber != userGuess);
-
-        System.out.println();
-        if (guessCount == 1) {
-            System.out.println("You answered number is right in " + guessCount + " try!");
+        System.out.println(" ");
+        if (guess == 1) {
+            System.out.println("You answered number is right in " + guess + " try!");
         } else {
-            System.out.println("You answered number is right in " + guessCount + " tries!");
+            System.out.println("You answered number is right in " + guess + " tries!");
         }
-        scoreBoard.add(guessCount);
-        System.out.println();
-    }
+        scoreBoard.add(guess);
+        System.out.println(" ");
 
+        menu(scoreBoard);
+    }
     public void displayScoreBoard() {
         System.out.println("--------------------");
         System.out.println("Score Board");
         System.out.println("--------------------");
-        if (scoreBoard.isEmpty()) {
-            System.out.println("No games played yet.");
-        } else {
-            Collections.sort(scoreBoard);
-            System.out.println("Your fastest games today out of all tries is:\n");
-            for (Integer score : scoreBoard) {
-                System.out.println("Finished the number game in " + score + " tries");
-            }
+        System.out.println("Your fastest games today out of all tries is: " +"\n");
+        Collections.sort(scoreBoard);
+        for (Integer scores : scoreBoard) {
+            System.out.println("Finished the number game in " + scores + " tries");
         }
-        System.out.println();
+        System.out.println(" ");
+        menu(scoreBoard);
     }
 }
